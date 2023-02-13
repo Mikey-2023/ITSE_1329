38e0be79b6d1676d193d88cca714c8e024f95e61
Mikey-2023 <mcamp@student.dallascollege.edu>
2023-02-12 20:33:00 -0600

    Created file salary.kt

---
+++ src/main/kotlin/unit02/salary.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val name = "Michael Camp"
+    var age = 22
+    var annualPay = 70000.00
+
+    print("My name is $name, my age is $age, and I hope to earn $$annualPay per year.")
+}
