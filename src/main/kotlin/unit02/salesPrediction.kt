cae4855ec2942ffde2a8c095636fe42dcfa26c32
Mikey-2023 <mcamp@student.dallascollege.edu>
2023-02-12 20:48:18 -0600

    Created file salesPrediction.kt

---
+++ src/main/kotlin/unit02/salesPrediction.kt
@@ -0,0 +1,4 @@
+fun main(){
+    var sales_EastCoast = 0.62 * 4600000
+    print("It's predicted that the East Coast sales division will generate $$sales_EastCoast if the company makes $4.6 million in sales.")
+}
