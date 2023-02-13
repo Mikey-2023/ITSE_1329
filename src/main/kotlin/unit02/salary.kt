165220b30faff578605af8341ce1cc15f3e58e95
Mikey-2023 <123129691+Mikey-2023@users.noreply.github.com>
2023-02-12 20:29:44 -0600

    Delete salary.kt

--- src/main/kotlin/unit02/salary.kt
+++
@@ -1,7 +0,0 @@
-fun main(){
-    var name = "Michael Camp"
-    var age = 22
-    var annualPay = 70000.00
-
-    print("My name is $name, my age is $age, and I hope to earn $$annualPay per year.")
-}
