8bc884c7d3e7ba76dc2024647807c7dbbd7f1503
Mikey-2023 <mcamp@student.dallascollege.edu>
2023-03-04 19:38:14 -0600

    Created file letterCounter.kt

---
+++ src/main/kotlin/unit04/letterCounter.kt
@@ -0,0 +1,16 @@
+fun main(){
+    print("Enter a string: ")
+    val userInput = readln()
+
+    print("Enter a single character: ")
+    val userChar = readln()[0]
+
+    var sum = 0
+    for (x in userInput){
+        if (x == userChar){
+            sum += 1
+        }
+    }
+
+    print("Sum: $sum")
+}
