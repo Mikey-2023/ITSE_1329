e1eb6447f793c5e9fb3890d7a8f1e65310958cbf
Mikey-2023 <mcamp@student.dallascollege.edu>
2023-03-29 12:31:24 -0500

    Created file primeTime.kt

---
+++ src/main/kotlin/unit05/primeTime.kt
@@ -0,0 +1,33 @@
+package unit05
+import kotlin.math.floor
+import kotlin.math.sqrt
+
+fun isNumberDivisible(number: Int, divisor: Int): Boolean {
+
+    return number % divisor == 0
+
+}
+
+fun isPrime(number: Int): Boolean {
+
+    if (number <= 1) {
+        return false
+    }
+
+    for (i in 2..floor(sqrt(number.toDouble())).toInt()) {
+        if (isNumberDivisible(number, i)) {
+            return false
+        }
+    }
+
+    return true
+
+}
+
+fun main(){
+
+    println(isPrime(6))
+    println(isPrime(13))
+    println(isPrime(8893))
+
+}
