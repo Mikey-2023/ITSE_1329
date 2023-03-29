13bd28b0c8e4b2b36e90b62ecc3825f27b1ffa0a
Mikey-2023 <mcamp@student.dallascollege.edu>
2023-03-29 17:00:00 -0500

    Created file recursiveFunction.kt

---
+++ src/main/kotlin/unit05/recursiveFunction.kt
@@ -0,0 +1,26 @@
+package unit05
+
+fun fibonacci(number: Int): Int{
+
+    return if (number >= 3) {
+        fibonacci(number-2) + fibonacci(number-1)
+    } else if (number > 0) {
+        1
+    } else {
+        0
+    }
+
+}
+
+fun main(){
+
+    println(fibonacci(1))
+    println(fibonacci(2))
+    println(fibonacci(3))
+    println(fibonacci(4))
+    println(fibonacci(5))
+    println(fibonacci(6))
+    println(fibonacci(7))
+    println(fibonacci(10))
+
+}
