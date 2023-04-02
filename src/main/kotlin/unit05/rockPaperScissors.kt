d1176fe4e67707dcb36988a68c5b9e4640d5c89e
Mikey-2023 <mcamp@student.dallascollege.edu>
2023-04-02 16:43:50 -0500

    Added documentation and function computerAnswer to rockPaperScissors.kt for readability

--- src/main/kotlin/unit05/rockPaperScissors.kt
+++ src/main/kotlin/unit05/rockPaperScissors.kt
@@ -8,7 +8,9 @@ fun printGameRules() {
 
 }
 
-
+/**
+ * Returns true if the user enters "y", and otherwise returns false
+ */
 fun wantsToPlay(): Boolean {
 
     print("Do you want to play? Type 'y' or 'n': ")
@@ -16,7 +18,9 @@ fun wantsToPlay(): Boolean {
 
 }
 
-
+/**
+ * Returns the player's choice only once they enter either "1", "2", or "3"
+ */
 fun validateAnswer(): Int {
 
     var playerChoice: Int? = null
@@ -43,16 +47,25 @@ fun validateAnswer(): Int {
 }
 
 
+fun computerAnswer(): Int {
+    return (1..3).random()
+}
+
+/**
+ * Returns both the player's and computer's choice as a pair of integers
+ * Uses functions validateAnswers and computerAnswer to get the choices
+ */
 fun getAnswers(): Pair<Int, Int> {
 
     print("Choose Rock, Paper or Scissors: ")
     val playerChoice = validateAnswer()
 
-    val computerChoice = (1..3).random()
+    val computerChoice = computerAnswer()
+
     val numsForRPS = mapOf(1 to "Rock", 2 to "Paper", 3 to "Scissors")
     println("The computer chose ${numsForRPS[computerChoice]}")
 
-    return Pair(playerChoice, computerChoice)  // First is player's answers , second is computer's
+    return Pair(playerChoice, computerChoice)  // First element is the player's choice, and the second is the computer's
 
 }
 
@@ -61,6 +74,9 @@ fun getAnswers(): Pair<Int, Int> {
 fun main(){
 
     printGameRules()
+
+    /* The map object below stores the combinations of rock/paper/scissors that result in the player winning, losing,
+       or tying with the computer */
     val mapResults = mapOf(
                             Pair(1,1) to "Tie!", Pair(2,2) to "Tie!", Pair(3,3) to "Tie!",
                             Pair(1,3) to "You win", Pair(2,1) to "You win", Pair(3, 2) to "You win",
